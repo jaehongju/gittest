8bbb488047da01a1b40fb7ac9f67ac6c3288211f
jaehong.ju <wnwoghd7839@naver.com>
2022-06-28 19:47:41 +0900

    3번째 변경  commit git 테스트

--- EmsAPI/src/main/java/kr/youhost/ems/api/common/BaseResult.java
+++ EmsAPI/src/main/java/kr/youhost/ems/api/common/BaseResult.java
@@ -11,8 +11,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-프로그램 변경 가가가가가가가가가 두번째
-
+// 3번째 변경
 
 @JsonInclude(Include.NON_NULL)
 @Data
